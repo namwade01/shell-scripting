befc19555b8f7b3253c38018b70e4412bf220726
Chandu <namwade.deveops@gmail.com>
2020-09-10 11:54:20 +0530

    modified Demo.sh

--- Demo.sh
+++ Demo.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-echo "testing shell scripting"
+var=10
+abc=chandu
+Float=2.3
+echo "var=$var, abc=$abc,Float=$Float"
+echo  "please enter ur name :" name
+read name
+echo -e "hello \e[32m$name \e[0m welcome to the RHEL"
+Array=(2 4 8 9)
+echo "index at 2=>:${Array[2]}"
+DATE=$(date +%F)
+echo "Todays date is $DATE"
