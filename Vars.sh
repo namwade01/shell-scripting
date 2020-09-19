b4202d1dbccc466788e24619e2141a89c0de8e9b
root <root@ip-172-31-37-197.us-east-2.compute.internal>
2020-09-19 05:54:27 +0000

    Vars github and azure repo

--- Vars.sh
+++ Vars.sh
@@ -12,3 +12,4 @@ DATE=$(date +%F)
 echo "Todays date is:  $DATE"
 echo "using git hub repo"
 echo "github connection establishe in azure repos"
+echo "connection between github and azure repe established"
